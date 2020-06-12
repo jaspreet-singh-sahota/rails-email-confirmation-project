d6acef7def2957e3fadbed94fda060f4df1699de
jaspreet-singh-sahota <jaspreetsinghjassi01@gmail.com>
2020-06-13 03:59:10 +0530

    modify env filr

--- app/mailers/application_mailer.rb
+++ app/mailers/application_mailer.rb
@@ -1,4 +1,4 @@
 class ApplicationMailer < ActionMailer::Base
-  default from: 'Test@sandbox0e9fef58dd3441f6875c880ce831cb8d.mailgun.org'
+  default from: ''
   layout 'mailer'
 end
--- config/environments/production.rb
+++ config/environments/production.rb
@@ -89,6 +89,8 @@ Rails.application.configure do
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
 
+  config.action_mailer.default_url_options = { host: 'email-comfirmation.herokuapp.com' }
+
   
   ActionMailer::Base.smtp_settings = {
     :port           => ENV['MAILGUN_SMTP_PORT'],
